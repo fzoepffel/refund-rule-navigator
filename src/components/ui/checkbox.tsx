50d4bf3d47420b6fc23bf4d626a5e1ec6edd9c17
Frederic Zoepffel <>
2025-05-21 09:42:44 +0200

    quick design fix

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -14,7 +14,7 @@ const Checkbox = React.forwardRef<
     ref={ref}
     className={cn(
       "peer h-4 w-4 shrink-0 rounded-sm ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-      indeterminate ? "bg-gray-500 text-white" : "data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      indeterminate ? "bg-gray-500 text-white" : "border border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
       className
     )}
     {...props}
